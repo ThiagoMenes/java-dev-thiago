4ff38c8117312472959664bf64980e70aa357d53
Thiago <thiagoesmael19@gmail.com>
2024-11-17 14:12:59 -0300

    first commit

---
+++ src/main/java/me/thiago/JavaDevThiagoApplication.java
@@ -0,0 +1,13 @@
+package me.thiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevThiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevThiagoApplication.class, args);
+	}
+
+}
