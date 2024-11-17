6cc9669a0973f152e380f40fefb80de5c97b00e7
Thiago <thiagoesmael19@gmail.com>
2024-11-17 19:39:11 -0300

    Adicionando mais detalhes na API RESTful

---
+++ src/main/java/me/thiago/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package me.thiago.domain.repository;
+
+import me.thiago.domain.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
